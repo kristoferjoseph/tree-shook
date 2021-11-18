1e146bd0d6a67222496e40ace103989d2bca13cf
kj <kristoferjoseph@gmail.com>
2021-11-17 16:33:19 -0800

    Works

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,11 +1,18 @@
 const html = require('@begin/enhance')()
 
-exports.handler = function http() {
+exports.handler = async function http() {
   return {
     statusCode: 200,
     headers: {
       'content-type': 'text/html; charset=utf8'
     },
-    body: html`<hello-world greeting="Halo Verld!"></hello-world>`
+    body: html`<hello-world>Halo Verld!</hello-world>
+<script>
+  Array.from(document.getElementsByTagName("template"))
+      .forEach(t => 'SCRIPT' === t.content.lastElementChild.nodeName
+      ? document.body.appendChild(t.content.lastElementChild)
+      :'')
+</script>
+    `
   }
 }
